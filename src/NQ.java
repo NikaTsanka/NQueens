6d52b52f88376f5d4ad4e2b47032fdb6f701a328
nikatsanka <nikatsanka@outlook.com>
2015-09-21 19:20:04 -0400

    First Commit 2

--- src/NQ.java
+++ src/NQ.java
@@ -4,10 +4,7 @@ public class NQ {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi");
-<<<<<<< HEAD
 		System.out.println("hello from intellij");
-=======
->>>>>>> origin/master
 	}
 
 }
